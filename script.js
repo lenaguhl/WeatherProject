1181e84737fffcab4f5fbd89ceae574428a5be69
lenaguhl <98180093+lenaguhl@users.noreply.github.com>
2022-02-11 21:48:48 +0100

    Create and connect script.js + add current date

---
+++ script.js
@@ -0,0 +1,89 @@
+let now = new Date();
+
+let currentDateTime = document.querySelector(".current-date");
+
+let weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let weekday = weekdays[now.getDay()];
+
+let dates = [
+  "1st",
+  "1st",
+  "2nd",
+  "3rd",
+  "4th",
+  "5th",
+  "6th",
+  "7th",
+  "8th",
+  "9th",
+  "10th",
+  "11th",
+  "12th",
+  "13th",
+  "14th",
+  "15th",
+  "16th",
+  "17th",
+  "18th",
+  "19th",
+  "20th",
+  "21st",
+  "22nd",
+  "23rd",
+  "24th",
+  "25th",
+  "26th",
+  "27th",
+  "28th",
+  "29th",
+  "30th",
+  "31st",
+];
+let date = dates[now.getDate()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+let year = now.getFullYear();
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+currentDateTime.innerHTML = `${weekday}, ${date} of ${month} ${year}`;
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return days[day];
+}
